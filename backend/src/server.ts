7ca886bdd7c520c41bee16493893757be3c820bb
Ahmed Hafez <ahmedhafez13121@gmail.com>
2024-03-29 14:46:13 +0200

    Add JSDoc to the server class

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,28 @@
 import fastify, { FastifyInstance } from 'fastify';
 import AppPlugins from './plugins/app.plugins';
 
+/**
+ * The main server class that manages Fastify application initialization, configuration, plugin registration, and startup.
+ */
 export default class Server {
   private app: FastifyInstance;
 
+  /**
+   * Creates a new Server instance.
+   *
+   * @param options (object, optional) - Configuration options to be passed to the Fastify instance during initialization.
+   */
   constructor(options = {}) {
     this.app = fastify(options);
   }
 
+  /**
+   * Starts the server by listening on a specified port and host.
+   *
+   * @param port (number) - The port number on which the server will listen for incoming requests.
+   * @param host (string) - The hostname or IP address on which the server will listen.
+   * @returns {Promise<void>} A Promise that resolves when the server is successfully listening.
+   */
   public async start(port: number, host: string): Promise<void> {
     this.setupAppHooks();
     this.setupErrorHooks();
@@ -22,12 +37,18 @@ export default class Server {
     });
   }
 
+  /**
+   * Sets up custom hooks for the Fastify application (e.g., request/response lifecycle hooks, global error handling).
+   */
   private setupAppHooks() {
     // TODO HANDLE AUTHENTICATION HOOK
     // this.app.addHook('onRequest', new AuthHook(this.app).handler);
   }
 
-  setupErrorHooks() {
+  /**
+   * Configures error handling mechanisms for the application (e.g., custom error handlers, logging).
+   */
+  private setupErrorHooks() {
     this.app.addHook('onError', (_request, _reply, error, done) => {
       // TODO: APPLY A PROBER ERROR HANDLING
       console.error({ error });
@@ -35,10 +56,13 @@ export default class Server {
     });
   }
 
-  registerAppPlugins() {
+  /**
+   * Registers application plugins with the Fastify instance.
+   */
+  private registerAppPlugins() {
     for (const AppPlugin of AppPlugins) {
-      this.app.register(async (instance, options, done) =>
-        new AppPlugin(instance, options, done).handler()
+      this.app.register(async (app, options, done) =>
+        new AppPlugin(app, options, done).handler()
       );
     }
   }
