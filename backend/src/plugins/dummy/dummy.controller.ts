b832fe1e688d1fa82025e0068a0bd242cf4add27
Ahmed Hafez <ahmedhafez13121@gmail.com>
2024-04-03 11:42:38 +0200

    Clean up

--- backend/src/plugins/dummy/dummy.controller.ts
+++
@@ -1,9 +0,0 @@
-import { FastifyReply, FastifyRequest } from 'fastify';
-
-class DummyController {
-  async dummyHandler(_request: FastifyRequest, reply: FastifyReply) {
-    reply.send({ hello: 'from DummyController.dummyHandler' });
-  }
-}
-
-export default DummyController;
